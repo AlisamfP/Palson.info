d15769e8aaf784c9affb0bac3ae0bc63dd5b2b12
alisamfp <eatliightning@gmail.com>
2017-09-19 10:28:42 -0700

    getting there, slowly but surely.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import photo from '../../photo.jpg';
 import './sidebar.css';
 
+
+
 class SideBar extends Component {
   render() {
     return (
         <div className="sidebar-container">
-          <img src={photo} className="sidebar-face" alt="my face" />
-          <div className="sidebar-info">
-            <p>Alisa Palson</p>
-            <p>Software Enginner</p>
-            <p>Design enthusiast</p>
+          <div className="sidebar--expanded">
+            <img src={photo} className="sidebar-face" alt="my face" />
+            <div className="sidebar-info">
+              <p>Alisa Palson</p>
+              <p>Software Enginner</p>
+              <p>Design enthusiast</p>
+            </div>
           </div>
         </div>
 
