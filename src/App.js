3dd87c4b6b80172262b7c257c24f5d48962299c0
alisamfp <eatliightning@gmail.com>
2017-09-19 12:28:28 -0700

    needs cleanup, dropdown working

--- src/App.js
+++ src/App.js
@@ -4,10 +4,34 @@ import SideBar from './components/sidebar/sidebar';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleSideBar = this.toggleSideBar.bind(this);
+    this.state = {
+      activeMenu: false
+    };
+  }
+
+  toggleSideBar() {
+    let sideBarState = !this.state.activeMenu
+    this.setState({
+      activeMenu: sideBarState
+    })
+  }
+
   render() {
+    let menu;
+    let {activeMenu} = this.state
+    if(activeMenu) {
+      menu = <div>HEY</div>
+    }
+    else {
+      menu = <SideBar></SideBar>
+    }
     return (
       <div className="main-wrapper">
-        <SideBar></SideBar>
+        <i className="fa fa-rocket" onClick={this.toggleSideBar} />
+        { menu }
         <Content></Content>
       </div>
     )
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -3,21 +3,19 @@ import photo from '../../photo.jpg';
 import './sidebar.css';
 
 
-
 class SideBar extends Component {
   render() {
     return (
-        <div className="sidebar-container">
-          <div className="sidebar--expanded">
-            <img src={photo} className="sidebar-face" alt="my face" />
-            <div className="sidebar-info">
-              <p>Alisa Palson</p>
-              <p>Software Enginner</p>
-              <p>Design enthusiast</p>
-            </div>
+      <div className="sidebar-container">
+        <div className="sidebar--expanded">
+          <img src={photo} className="sidebar-face" alt="my face" />
+          <div className="sidebar-info">
+            <p>Alisa Palson</p>
+            <p>Certified human being</p>
+            <p>Javascript Vixen.</p>
           </div>
         </div>
-
+      </div>
     )
   }
 }
